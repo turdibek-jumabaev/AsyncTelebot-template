c9b216499f78564b64cf583838da0cdab0584b3e
Turdibek <turdibekdev05@gmail.com>
2022-02-17 21:47:37 +0500

    AsyncTelebot-template

--- loader.py
+++ loader.py
@@ -1,5 +1,5 @@
-from telebot.async_telebot import AsyncTeleBot
+from telebot.async_telebot import AsyncTeleBot, ExceptionHandler
 from data.config import BOT_TOKEN
 
 
-bot = AsyncTeleBot(BOT_TOKEN)
+bot = AsyncTeleBot(BOT_TOKEN, exception_handler=ExceptionHandler())
