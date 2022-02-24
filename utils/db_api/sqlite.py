2b0bb9470e388f5f0ce38867267b61c59ab0587d
Turdibek <turdibekdev05@gmail.com>
2022-02-24 11:15:41 +0500

    SQLite

--- utils/db_api/sqlite.py
+++ utils/db_api/sqlite.py
@@ -1,31 +1,32 @@
 import sqlite3
 
-class Datebase:
-    def __init__(self, path_to_db='main.db'):
+
+class Database:
+    def __init__(self, path_to_db="main.db"):
         self.path_to_db = path_to_db
-    
+
     @property
     def connection(self):
         return sqlite3.connect(self.path_to_db)
-    
+
     def execute(self, sql: str, parameters: tuple = None, fetchone=False, fetchall=False, commit=False):
         if not parameters:
             parameters = ()
         connection = self.connection
         connection.set_trace_callback(logger)
-        data = None
         cursor = connection.cursor()
+        data = None
         cursor.execute(sql, parameters)
 
         if commit:
             connection.commit()
-        if fetchone:
-            data = connection.fecthone()
         if fetchall:
-            data = connection.fetchall()
+            data = cursor.fetchall()
+        if fetchone:
+            data = cursor.fetchone()
         connection.close()
-        return data 
-    
+        return data
+
     def create_table_users(self):
         sql = """
         CREATE TABLE Users (
@@ -37,49 +38,49 @@ class Datebase:
             );
 """
         self.execute(sql, commit=True)
-    
+
     @staticmethod
     def format_args(sql, parameters: dict):
         sql += " AND ".join([
             f"{item} = ?" for item in parameters
         ])
         return sql, tuple(parameters.values())
-    
+
     def add_user(self, id: int, name: str, email: str = None, language: str = 'uz'):
+        # SQL_EXAMPLE = "INSERT INTO Users(id, Name, email) VALUES(1, 'John', 'John@gmail.com')"
 
         sql = """
         INSERT INTO Users(id, Name, email, language) VALUES(?, ?, ?, ?)
         """
         self.execute(sql, parameters=(id, name, email, language), commit=True)
-    
+
     def select_all_users(self):
         sql = """
         SELECT * FROM Users
         """
         return self.execute(sql, fetchall=True)
-    
+
     def select_user(self, **kwargs):
+        # SQL_EXAMPLE = "SELECT * FROM Users where id=1 AND Name='John'"
         sql = "SELECT * FROM Users WHERE "
         sql, parameters = self.format_args(sql, kwargs)
 
         return self.execute(sql, parameters=parameters, fetchone=True)
-    
+
     def count_users(self):
-        sql = """
-        select count(*) from Users;
-        """
-        return self.execute(sql=sql, fetchone=True)
-    
+        return self.execute("SELECT COUNT(*) FROM Users;", fetchone=True)
+
     def update_user_email(self, email, id):
+        # SQL_EXAMPLE = "UPDATE Users SET email=mail@gmail.com WHERE id=12345"
 
         sql = f"""
         UPDATE Users SET email=? WHERE id=?
         """
-        command = self.execute(sql, parameters=(email, id), commit=True)
-        return command
+        return self.execute(sql, parameters=(email, id), commit=True)
 
     def delete_users(self):
         self.execute("DELETE FROM Users WHERE TRUE", commit=True)
+
 
 def logger(statement):
     print(f"""
